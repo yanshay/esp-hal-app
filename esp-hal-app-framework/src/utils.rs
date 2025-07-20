2e87483a77c45f0aab3fe4c1698c78f4cf710ca6
yanshay <yanshay>
2025-07-20 18:04:52 +0300

    add (commented) log of size of static allocs

--- esp-hal-app-framework/src/utils.rs
+++ esp-hal-app-framework/src/utils.rs
@@ -2,6 +2,8 @@
 macro_rules! mk_static {
     ($t:ty,$val:expr) => {{
         static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        // const SIZE: usize = core::mem::size_of::<$t>();
+        // debug!(">>>{} -> {}", core::any::type_name::<$t>(), SIZE);
         #[deny(unused_attributes)]
         let x = STATIC_CELL.uninit().write(($val));
         x
