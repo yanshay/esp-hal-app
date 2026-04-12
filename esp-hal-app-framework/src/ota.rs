8707daecf4dafed41b88294640804576229d7f79
yanshay <yanshay>
2026-04-12 18:24:59 +0300

    properly close connections in ota

--- esp-hal-app-framework/src/ota.rs
+++ esp-hal-app-framework/src/ota.rs
@@ -209,206 +209,209 @@ pub async fn run_ota(
         SocketAddr::new(core::net::IpAddr::V4(addr), 443),
     ));
 
-    // Get ota.toml
+    'block: {
+        // Get ota.toml
 
-    let toml_filename = format!("{ota_path}{ota_toml_filename}");
+        let toml_filename = format!("{ota_path}{ota_toml_filename}");
 
-    info!("Fetching OTA metadata from {toml_filename}");
-    report(Report::Status, "Fetching firmware metadata");
-    if let Err(err) = conn
-        .initiate_request(
-            true,
-            edge_http::Method::Get,
-            &toml_filename,
-            &[("Host", ota_domain)],
-        )
-        .await
-    {
-        report(Report::Failure, "Failed to initiate request for metadata");
-        error!("Error: {err:?}");
-        return;
-    }
-
-    if let Err(err) = conn.initiate_response().await {
-        report(Report::Failure, "Failed to fetch response for metadata");
-        error!("Error: {err:?}");
-        return;
-    };
-
-    let headers = match conn.headers() {
-        Ok(headers) => headers,
-        Err(err) => {
-            report(Report::Failure, "Failed to read resopnse headers");
-            info!("Error: {err}");
-            return;
+        info!("Fetching OTA metadata from {toml_filename}");
+        report(Report::Status, "Fetching firmware metadata");
+        if let Err(err) = conn
+            .initiate_request(
+                true,
+                edge_http::Method::Get,
+                &toml_filename,
+                &[("Host", ota_domain)],
+            )
+            .await
+        {
+            report(Report::Failure, "Failed to initiate request for metadata");
+            error!("Error: {err:?}");
+            break 'block;
         }
-    };
 
-    let status_code = headers.code;
-    if status_code != 200 {
-        report(Report::Failure, "Failed to fetch firmware metadata");
-        return;
-    }
+        if let Err(err) = conn.initiate_response().await {
+            report(Report::Failure, "Failed to fetch response for metadata");
+            error!("Error: {err:?}");
+            break 'block;
+        };
 
-    // TODO - loop to read until buffer full or nothing to read
-    let Ok(len) = conn.read(&mut *data_buf).await else {
-        report(Report::Failure, "Failed to read response");
-        return;
-    };
+        let headers = match conn.headers() {
+            Ok(headers) => headers,
+            Err(err) => {
+                report(Report::Failure, "Failed to read resopnse headers");
+                info!("Error: {err}");
+                break 'block;
+            }
+        };
 
-    let toml = core::str::from_utf8(&data_buf[..len]).unwrap_or_default();
-    info!("Firmware metadata:\n{}", toml.trim());
+        let status_code = headers.code;
+        if status_code != 200 {
+            report(Report::Failure, "Failed to fetch firmware metadata");
+            break 'block;
+        }
 
-    let mut filename = None;
-    let mut crc32 = None;
-    let mut version = None;
-    let mut filesize = None;
+        // TODO - loop to read until buffer full or nothing to read
+        let Ok(len) = conn.read(&mut *data_buf).await else {
+            report(Report::Failure, "Failed to read response");
+            break 'block;
+        };
 
-    for line in toml.lines() {
-        if let Some((key, value)) = line.split_once('=') {
-            match key.trim() {
-                "filename" => filename = Some(value.trim().trim_matches('"')),
-                "crc32" => crc32 = Some(u32::from_str_radix(value.trim().trim_matches('"'), 16)),
-                "filesize" => filesize = Some(value.trim().trim_matches('"').parse::<u32>()),
-                "version" => version = Some(value.trim().trim_matches('"')),
-                _ => (), // Ignore unknown keys
+        let toml = core::str::from_utf8(&data_buf[..len]).unwrap_or_default();
+        info!("Firmware metadata:\n{}", toml.trim());
+
+        let mut filename = None;
+        let mut crc32 = None;
+        let mut version = None;
+        let mut filesize = None;
+
+        for line in toml.lines() {
+            if let Some((key, value)) = line.split_once('=') {
+                match key.trim() {
+                    "filename" => filename = Some(value.trim().trim_matches('"')),
+                    "crc32" => {
+                        crc32 = Some(u32::from_str_radix(value.trim().trim_matches('"'), 16))
+                    }
+                    "filesize" => filesize = Some(value.trim().trim_matches('"').parse::<u32>()),
+                    "version" => version = Some(value.trim().trim_matches('"')),
+                    _ => (), // Ignore unknown keys
+                }
             }
         }
-    }
-    let (Some(filename), Some(Ok(crc32)), Some(version), Some(Ok(filesize))) =
-        (filename, crc32, version, filesize)
-    else {
-        report(Report::Failure, "Something is wrong with firmware metadata");
-        return;
-    };
+        let (Some(filename), Some(Ok(crc32)), Some(version), Some(Ok(filesize))) =
+            (filename, crc32, version, filesize)
+        else {
+            report(Report::Failure, "Something is wrong with firmware metadata");
+            break 'block;
+        };
 
-    let new_semver = match Version::parse(version) {
-        Ok(v) => v,
-        Err(_) => {
+        let new_semver = match Version::parse(version) {
+            Ok(v) => v,
+            Err(_) => {
+                report(
+                    Report::Failure,
+                    "Version number in firmware metadata is invalid",
+                );
+                break 'block;
+            }
+        };
+
+        let newer = if let Ok(cur_semver) = Version::parse(cur_version) {
+            new_semver > cur_semver
+        } else {
+            false
+        };
+
+        if !newer {
+            report(
+                Report::Complete,
+                &format!(
+                    "Current firmware version {} is up to date",
+                    framework.borrow().settings.app_cargo_pkg_version
+                ),
+            );
+            report(Report::Version(version, false), "Version is up to date");
+            break 'block;
+        } else {
+            report(Report::Version(version, true), "Version is behind");
+        }
+
+        if ota_request == OtaRequest::CheckVersion {
+            break 'block;
+        }
+
+        // Fetch the bin file
+
+        report(Report::Status, "Downloading firmware");
+        let bin_filename = format!("{}{}", ota_path, filename);
+        if let Err(e) = conn
+            .initiate_request(
+                true,
+                edge_http::Method::Get,
+                &bin_filename,
+                &[("Host", ota_domain)],
+            )
+            .await
+        {
             report(
                 Report::Failure,
-                "Version number in firmware metadata is invalid",
+                &format!("Failed to initiate request for firmware {e:?}"),
             );
-            return;
+            break 'block;
         }
-    };
 
-    let newer = if let Ok(cur_semver) = Version::parse(cur_version) {
-        new_semver > cur_semver
-    } else {
-        false
-    };
-
-    if !newer {
-        report(
-            Report::Complete,
-            &format!(
-                "Current firmware version {} is up to date",
-                framework.borrow().settings.app_cargo_pkg_version
-            ),
-        );
-        report(Report::Version(version, false), "Version is up to date");
-        return;
-    } else {
-        report(Report::Version(version, true), "Version is behind");
-    }
-
-    if ota_request == OtaRequest::CheckVersion {
-        return;
-    }
-
-    // Fetch the bin file
-
-    report(Report::Status, "Downloading firmware");
-    let bin_filename = format!("{}{}", ota_path, filename);
-    if let Err(e) = conn
-        .initiate_request(
-            true,
-            edge_http::Method::Get,
-            &bin_filename,
-            &[("Host", ota_domain)],
-        )
-        .await
-    {
-        report(
-            Report::Failure,
-            &format!("Failed to initiate request for firmware {e:?}"),
-        );
-        return;
-    }
-
-    if let Err(e) = conn.initiate_response().await {
-        report(
-            Report::Failure,
-            &format!("Failed to fetch response for metadata {e:?}"),
-        );
-        return;
-    }
-
-    let status_code = conn.headers().unwrap().code;
-    info!("Response code {}", status_code);
-    if status_code != 200 {
-        report(Report::Failure, "Failed downloading firmware");
-        return;
-    }
-
-    // start OTA
-
-    let mut ota = match Ota::new(FlashStorage::new()) {
-        Ok(v) => v,
-        Err(_) => {
-            report(Report::Failure, "Error initializing flashing");
-            return;
+        if let Err(e) = conn.initiate_response().await {
+            report(
+                Report::Failure,
+                &format!("Failed to fetch response for metadata {e:?}"),
+            );
+            break 'block;
         }
-    };
-    ota.ota_begin(filesize, crc32).unwrap_or_else(|e| {
-        report(Report::Failure, &format!("Failed to start OTA: {e:?}"));
-    });
 
-    debug!("Starting firmware download");
-    let mut bytes_read = 0;
-    let start_time = embassy_time::Instant::now();
-    let mut reported_on_sec_since_start = 0;
-    let mut x = 0;
-    let mut sec_since_start;
-    loop {
-        let bytes_to_read = data_buf
-            .len()
-            .min((filesize - bytes_read).try_into().unwrap());
+        let status_code = conn.headers().unwrap().code;
+        info!("Response code {}", status_code);
+        if status_code != 200 {
+            report(Report::Failure, "Failed downloading firmware");
+            break 'block;
+        }
 
-        if conn
-            .read_exact(&mut data_buf[..bytes_to_read])
-            .await
-            .is_ok()
-        {
-            bytes_read += bytes_to_read as u32;
+        // start OTA
 
-            if bytes_to_read == 0 {
-                error!("Binary File smaller than expected");
-                break;
+        let mut ota = match Ota::new(FlashStorage::new()) {
+            Ok(v) => v,
+            Err(_) => {
+                report(Report::Failure, "Error initializing flashing");
+                break 'block;
             }
+        };
+        ota.ota_begin(filesize, crc32).unwrap_or_else(|e| {
+            report(Report::Failure, &format!("Failed to start OTA: {e:?}"));
+        });
 
-            let res = ota.ota_write_chunk(&data_buf[..bytes_to_read]);
+        debug!("Starting firmware download");
+        let mut bytes_read = 0;
+        let start_time = embassy_time::Instant::now();
+        let mut reported_on_sec_since_start = 0;
+        let mut x = 0;
+        let mut sec_since_start;
+        loop {
+            let bytes_to_read = data_buf
+                .len()
+                .min((filesize - bytes_read).try_into().unwrap());
 
-            match res {
-                Ok(true) => {
-                    let res = ota.ota_flush(false, true);
-                    sec_since_start = start_time.elapsed().as_secs();
-                    debug!(
+            if conn
+                .read_exact(&mut data_buf[..bytes_to_read])
+                .await
+                .is_ok()
+            {
+                bytes_read += bytes_to_read as u32;
+
+                if bytes_to_read == 0 {
+                    error!("Binary File smaller than expected");
+                    break;
+                }
+
+                let res = ota.ota_write_chunk(&data_buf[..bytes_to_read]);
+
+                match res {
+                    Ok(true) => {
+                        let res = ota.ota_flush(false, true);
+                        sec_since_start = start_time.elapsed().as_secs();
+                        debug!(
                         "Finished: {x}: {sec_since_start} secs, {bytes_read} {bytes_read} {:.0}%",
                         100.0
                     );
-                    info!(
-                        "Download & Flash time: {}ms",
-                        start_time.elapsed().as_millis()
-                    );
-                    if let Err(e) = res {
-                        report(Report::Failure, &format!("Ota flush error: {e:?}"));
-                        break;
-                    }
+                        info!(
+                            "Download & Flash time: {}ms",
+                            start_time.elapsed().as_millis()
+                        );
+                        if let Err(e) = res {
+                            report(Report::Failure, &format!("Ota flush error: {e:?}"));
+                            break;
+                        }
 
-                    for countdown in 0..5 {
-                        report(
+                        for countdown in 0..5 {
+                            report(
                             Report::Success,
                             &format!(
                                 "Firmware version {} flashed successfully\nRestarting {} in {} seconds",
@@ -417,31 +420,32 @@ pub async fn run_ota(
                                 5 - countdown
                             ),
                         );
-                        Timer::after_millis(1000).await;
+                            Timer::after_millis(1000).await;
+                        }
+                        esp_hal::system::software_reset();
                     }
-                    esp_hal::system::software_reset();
+                    Err(e) => {
+                        report(Report::Failure, &format!("Flashing error: {e:?}"));
+                        break;
+                    }
+                    _ => {}
                 }
-                Err(e) => {
-                    report(Report::Failure, &format!("Flashing error: {e:?}"));
-                    break;
+                sec_since_start = start_time.elapsed().as_secs();
+                if sec_since_start >= reported_on_sec_since_start {
+                    let progress_percent = ota.get_ota_progress() * 100.0;
+                    report(
+                        Report::Status,
+                        &format!(
+                            "Downloading/Flashing {} version {}\n{sec_since_start} secs, {:.0}%",
+                            framework.borrow().settings.app_cargo_pkg_name,
+                            new_semver,
+                            progress_percent
+                        ),
+                    );
+                    reported_on_sec_since_start = sec_since_start + 1;
                 }
-                _ => {}
+                x += 1;
             }
-            sec_since_start = start_time.elapsed().as_secs();
-            if sec_since_start >= reported_on_sec_since_start {
-                let progress_percent = ota.get_ota_progress() * 100.0;
-                report(
-                    Report::Status,
-                    &format!(
-                        "Downloading/Flashing {} version {}\n{sec_since_start} secs, {:.0}%",
-                        framework.borrow().settings.app_cargo_pkg_name,
-                        new_semver,
-                        progress_percent
-                    ),
-                );
-                reported_on_sec_since_start = sec_since_start + 1;
-            }
-            x += 1;
         }
     }
     conn.close().await.ok();
